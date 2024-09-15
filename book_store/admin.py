434e8a8d1f00cc083794888c886a9cfb10da8734
YogeshRajgure <yogeshrajgure.vraj@gmail.com>
2024-09-15 17:58:34 +0530

    relationship module complete

--- book_store/admin.py
+++ book_store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Address, Country
 
 # Register your models here.
 
@@ -9,4 +9,10 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author")
 
 
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ("first_name", "last_name")
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
