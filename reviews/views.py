b2ed6b5215a43d919cce0a74e8e67c5d15676f32
YogeshRajgure <yogeshrajgure.vraj@gmail.com>
2024-09-20 13:01:26 +0530

    forms module - using model form for easier access and easier updating

--- reviews/views.py
+++ reviews/views.py
@@ -25,11 +25,7 @@ def review(request):
         form = ReviewForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
-            review = Review(user_name=form.cleaned_data['user_name'], 
-                            review_text=form.cleaned_data['review_text'], 
-                            rating=form.cleaned_data['ratings']
-                            )
-            review.save()
+            form.save()
             return HttpResponseRedirect("thank-you")
     else: 
         form = ReviewForm()
